f87d2a4b4d68a94fc51c9e9d6f49799424c8d733
ngassier <gassiernicolas@gmail.com>
2018-12-15 16:02:21 +0100

    add documentation with swagger

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
@@ -1,8 +1,10 @@
 package com.ecommerce.microcommerce.web.exceptions;
 
+import io.swagger.annotations.Api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Api(description = "API pour gérer les exceptions des produits gratuit")
 @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
 public class ProduitGratuitException extends RuntimeException {
 
